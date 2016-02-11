25bf954c60932e6a0e8ce111aec193064a8734e5
ZotovaIrina <ZotovaIrina@users.noreply.github.com>
2016-02-11 17:29:38 +0300

    Delivering notification to the user. Use the local notification plugin to put a notification into the device's notification bar. Use the toast plugin to show a short message on the screen to alert the user.

--- Ionic/conFusion/www/js/app.js
+++ Ionic/conFusion/www/js/app.js
@@ -22,7 +22,7 @@ angular.module('conFusion', ['ionic', 'ngCordova', 'conFusion.controllers','conF
 
       $timeout(function(){
         $cordovaSplashscreen.hide();
-      },5000);
+      },4000);
 
     });
 
